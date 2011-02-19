7fedc9b63297b3451f28ee9b92e73dac89226b76
liserdarts_cp <116813+liserdarts@users.noreply.github.com>
2011-02-19 11:28:43 +0000

    Added a second sample for the weld joint and a sample menu

---
+++ XamlPhysics.Samples/SampleMenu.xaml.vb
@@ -0,0 +1,35 @@
+﻿Partial Public Class SampleMenu
+    Inherits Page
+
+    Public Class SampleInfo
+        
+        Public Sub New(Type As Type)
+            Me.Type = Type
+        End Sub
+
+        Public Property Type As Type
+        Public ReadOnly Property Name() As String
+            Get
+                Return Type.Name
+            End Get
+        End Property
+        Public ReadOnly Property Path() As String
+            Get
+                Return "/Samples/" & Type.FullName
+            End Get
+        End Property
+    End Class
+
+    Public Sub New()
+        InitializeComponent
+    End Sub
+
+    Protected Overrides Sub OnNavigatedTo(e As Windows.Navigation.NavigationEventArgs)
+        Dim Samples As New List(Of SampleInfo)
+        Samples.Add(New SampleInfo(GetType(Sample1)))
+        Samples.Add(New SampleInfo(GetType(Sample2)))
+
+        UxSamples.ItemsSource = Samples
+    End Sub
+
+End Class
