646017e3cf5fdbe94cf167bca30aa678058d326e
liserdarts_cp <SND\liserdarts_cp>
2011-02-19 11:28:43 +0000

    Added a second sample for the weld joint and a sample menu

--- XamlPhysics.Samples/MainPage.xaml.vb
+++ XamlPhysics.Samples/MainPage.xaml.vb
@@ -2,23 +2,7 @@
     Inherits UserControl
 
     Public Sub New()
-        InitializeComponent()
-    End Sub
-
-    Dim WithEvents GameLoop As New GameLoop
-
-    Private Sub MainPage_Loaded(sender As Object, e As Windows.RoutedEventArgs) Handles Me.Loaded
-        GameLoop.Interval = TimeSpan.FromMilliseconds(20)
-        GameLoop.Start
-    End Sub
-
-    Private Sub GameLoop_Tick(sender As Object, e As GameLoop.TickEventArgs) Handles GameLoop.Tick
-        Dispatcher.BeginInvoke(AddressOf Tick)
-    End Sub
-    Private Sub Tick()
-        UxBox.World.Step(GameLoop.Interval.TotalSeconds)
-        UxBox.Update(GameLoop.Interval)
-        UxBox.UpdateUI(GameLoop.Interval)
+        InitializeComponent
     End Sub
 
 End Class
