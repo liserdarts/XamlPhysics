e58e2122ad48a7232b27db2bfcaa6e1fdc3bd9f0
liserdarts_cp <116813+liserdarts@users.noreply.github.com>
2011-02-25 22:46:19 +0000

    Created 0.1 release

---
+++ Releases/0.1/PhysicalGeometry.vb
@@ -0,0 +1,71 @@
+﻿'Copyright (c) 2011, Nicholas Avery
+'Licensed under the Microsoft Public License (Ms-PL)
+'you may not use this file except in compliance with the License.
+'You may obtain a copy of the license at http://xamlphysics.codeplex.com/license
+
+''' <summary>
+''' The base class for all geometry classes
+''' </summary>s
+Public MustInherit Class PhysicalGeometry
+    
+    Event Collide(Sender As Object, E As CollideEventArgs)
+    Protected Overridable Function OnCollide(FixtureA As FarseerPhysics.Dynamics.Fixture, FixtureB As FarseerPhysics.Dynamics.Fixture, Manifold As FarseerPhysics.Dynamics.Contacts.Contact) As Boolean
+        Dim E As New CollideEventArgs(FixtureA, FixtureB, Manifold)
+        RaiseEvent Collide(Me, E)
+        Return Not E.Cancel
+    End Function
+
+    Event Separate(Sender As Object, E As CollideEventArgs)
+    Protected Overridable Sub OnSeparate(FixtureA As FarseerPhysics.Dynamics.Fixture, FixtureB As FarseerPhysics.Dynamics.Fixture)
+        Dim E As New CollideEventArgs(FixtureA, FixtureB)
+        RaiseEvent separate(Me, E)
+    End Sub
+
+    Public Property FrictionCoefficient() As Double = 0.2
+    
+    Public Property RestitutionCoefficient() As Double = 0.5
+    
+    Public Property CollisionCategory() As FarseerPhysics.Dynamics.Category = FarseerPhysics.Dynamics.Category.All
+
+    Public Property CollidesWith() As CollisionCategoryCollection
+    
+    ''' <summary>
+    ''' The <c>PhysicsBox</c> this geometry is in
+    ''' </summary>
+    Public Property Box As PhysicalBox
+
+    ''' <summary>
+    ''' The <c>Shape</c> object from the Farseer Physics Engine
+    ''' </summary>
+    Public Property Geom As FarseerPhysics.Collision.Shapes.Shape
+
+    ''' <summary>
+    ''' The <c>Fixture</c> object from the Farseer Physics Engine
+    ''' </summary>
+    Public Property Fixture As FarseerPhysics.Dynamics.Fixture
+    
+    ''' <summary>
+    ''' Creates the <c>Geom</c> and <c>Fixture</c> objects
+    ''' </summary>
+    Public Overridable Sub Initialize(Body As PhysicalBody)
+        CreateGeom(Body)
+        SetGeomProperties(Body)
+
+        Fixture = Body.Body.CreateFixture(Geom)
+        Fixture.Friction = FrictionCoefficient
+        Fixture.Restitution = RestitutionCoefficient
+        Fixture.CollisionFilter.CollisionCategories = CollisionCategory
+        If CollidesWith IsNot Nothing Then
+            Fixture.CollisionFilter.CollidesWith = CollidesWith.GetCombinedValue
+        End If
+
+        Fixture.OnCollision = New FarseerPhysics.Dynamics.OnCollisionEventHandler(AddressOf OnCollide)
+        Fixture.OnSeparation = New FarseerPhysics.Dynamics.OnSeparationEventHandler(AddressOf OnSeparate)
+    End Sub
+    
+    Protected MustOverride Sub CreateGeom(Body As PhysicalBody)
+    
+    Protected Overridable Sub SetGeomProperties(Body As PhysicalBody)
+    End Sub
+    
+End Class
---
+++ Releases/0.1/SliderJoint.vb
@@ -0,0 +1,59 @@
+﻿'Copyright (c) 2011, Nicholas Avery
+'Licensed under the Microsoft Public License (Ms-PL)
+'you may not use this file except in compliance with the License.
+'You may obtain a copy of the license at http://xamlphysics.codeplex.com/license
+
+''' <summary>
+''' Constrains two points on two bodies
+''' to remain at a fixed distance from each other. You can view
+''' this as a massless, rigid rod.
+''' </summary>
+Public Class SliderJoint
+    Inherits DoubleJoint
+    
+    ''' <summary>
+    ''' The minimum distance between the two bodies
+    ''' </summary>
+    ''' <remarks>This can't be changed after the simulation is started.</remarks>
+    Public Property Min() As Double
+    
+    ''' <summary>
+    ''' The maximum distance between the two bodies
+    ''' </summary>
+    ''' <remarks>This can't be changed after the simulation is started.</remarks>
+    Public Property Max() As Double
+    
+    ''' <summary>
+    ''' The point attached to the first body
+    ''' </summary>
+    ''' <remarks>This can't be changed after the simulation is started.</remarks>
+    Public Property Anchor1() As Point
+    
+    ''' <summary>
+    ''' The point attached to the second body
+    ''' </summary>
+    Public Property Anchor2() As Point
+    
+    ''' <summary>
+    ''' Gets the joint object from the Farseer Physics Engine
+    ''' </summary>
+    Public Shadows Property Joint() As FarseerPhysics.Dynamics.Joints.SliderJoint
+        Get
+            Return MyBase.Joint
+        End Get
+        Set
+            MyBase.Joint = Value
+        End Set
+    End Property
+    
+    Protected Overrides Sub CreateJoint()
+        Dim BodyA = PhysicalBox.GetBody(Body).Body
+        Dim BodyB = PhysicalBox.GetBody(Body2).Body
+        Dim VectorA = Box.PointToMeter(Anchor1.X, Anchor1.Y)
+        Dim VectorB = Box.PointToMeter(Anchor2.X, Anchor2.Y)
+
+        Joint = New FarseerPhysics.Dynamics.Joints.SliderJoint(BodyA, BodyB, VectorA, VectorB, Min, Max)
+        Joint.Frequency = 100
+    End Sub
+    
+End Class
