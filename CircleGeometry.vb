1fc8e73ec0e06484856e535cf705644e4b212133
liserdarts_cp <SND\liserdarts_cp>
2011-02-25 09:13:15 +0000

    Created CircleGeometry

---
+++ CircleGeometry.vb
@@ -0,0 +1,13 @@
+﻿Public Class CircleGeometry
+    Inherits PhysicalGeometry
+    
+    Public Property Radius() As Double
+    Public Property Position() As Point
+
+    Protected Overrides Sub CreateGeom(Body As PhysicalBody)
+        Dim Shape = New FarseerPhysics.Collision.Shapes.CircleShape(Box.PixelToMeter(Radius), 1)
+        Shape.Position = Box.PointToMeter(Position)
+        Geom = Shape
+    End Sub
+    
+End Class
