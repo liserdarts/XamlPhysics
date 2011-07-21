3426d1efa6e83f331c6e2c6a53b590749d749837
liserdarts_cp <SND\liserdarts_cp>
2011-07-21 01:21:52 +0000

    Moved the version attributes to a new file

--- Trunk/My Project/AssemblyInfo.vb
+++ Trunk/My Project/AssemblyInfo.vb
@@ -2,13 +2,8 @@
 Imports System.Reflection
 Imports System.Runtime.InteropServices
 
-' General Information about an assembly is controlled through the following 
-' set of attributes. Change these attribute values to modify the information
-' associated with an assembly.
-
-' Review the values of the assembly attributes
 <Assembly: AssemblyTitle("XamlPhysics")> 
-<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyDescription("XmlPhysics for Silverlight and WP7")> 
 <Assembly: AssemblyCompany("Liserd Arts")> 
 <Assembly: AssemblyProduct("XamlPhysics")> 
 <Assembly: AssemblyCopyright("Copyright © Liserd Arts 2011")> 
@@ -18,17 +13,3 @@ Imports System.Runtime.InteropServices
 
 'The following GUID is for the ID of the typelib if this project is exposed to COM
 <Assembly: Guid("010e1ece-d171-45ab-9816-e8a628c612c7")> 
-
-' Version information for an assembly consists of the following four values:
-'
-'      Major Version
-'      Minor Version 
-'      Build Number
-'      Revision
-'
-' You can specify all the values or you can default the Build and Revision Numbers 
-' by using the '*' as shown below:
-' <Assembly: AssemblyVersion("1.0.*")> 
-
-<Assembly: AssemblyVersion("0.4.0")> 
-<Assembly: AssemblyFileVersion("0.4.0")> 
