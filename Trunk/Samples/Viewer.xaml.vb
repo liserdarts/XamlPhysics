7ef7515a4dbf5cfc45808704ddecdfec9743cdfc
liserdarts_cp <SND\liserdarts_cp>
2011-07-21 18:45:11 +0000

    Moved the GameLoop out of the samples and into Xaml Physics. Added a clock property to PhysicalBox so it can attach directly to an instance of GameLoop.

--- Trunk/Samples/Viewer.xaml.vb
+++ Trunk/Samples/Viewer.xaml.vb
@@ -6,12 +6,10 @@
     End Sub
 
     Public Shared AutoPlay As Boolean = True
-    Dim WithEvents GameLoop As New GameLoop
 
     Protected Overrides Sub OnNavigatedTo(e As Windows.Navigation.NavigationEventArgs)
         [Stop]
 
-        GameLoop.Interval = TimeSpan.FromMilliseconds(20)
         If AutoPlay Then
             Play
         End If
@@ -25,22 +23,19 @@
     End Sub
 
     Private Sub Play()
-        GameLoop.Start
+        UxBox.Clock.Start
         UxPlay.IsEnabled = False
         UxPause.IsEnabled = True
         UxStop.IsEnabled = True
     End Sub
     Private Sub Pause() Handles UxPause.Click
-        Lock.Set
-        GameLoop.Stop(True)
+        UxBox.Clock.Stop(False)
         UxPlay.IsEnabled = True
         UxPause.IsEnabled = False
         UxStop.IsEnabled = True
     End Sub
     Private Sub [Stop]() Handles UxStop.Click
-        Active = False
-        Lock.Set
-        GameLoop.Stop(True)
+        UxBox.Clock.Stop(False)
         UxBox.Reset
         
         UxPlay.IsEnabled = True
@@ -54,25 +49,6 @@
         UxSampleContainer.Children.Add(Sample)
     End Sub
 
-    Dim Active As Boolean
-    Dim Lock As New System.Threading.ManualResetEvent(False)
-    Private Sub GameLoop_Tick(sender As Object, e As GameLoop.TickEventArgs) Handles GameLoop.Tick
-        Static Count As Integer = 0
-        Count = Count + 1
-        If Count > 10 Then
-            Lock.Reset
-            Dispatcher.BeginInvoke(AddressOf Tick)
-            Lock.WaitOne
-        End If
-    End Sub
-    Private Sub Tick()
-        If Not GameLoop.IsRunning Then Return
-        UxBox.World.Step(GameLoop.Interval.TotalSeconds)
-        UxBox.Update(GameLoop.Interval)
-        UxBox.UpdateUI(GameLoop.Interval)
-        Lock.Set
-    End Sub
-
     Private Sub UxPlay_Click(sender As Object, e As Windows.RoutedEventArgs) Handles UxPlay.Click
         Play
     End Sub
@@ -80,6 +56,6 @@
         Pause
     End Sub
     Private Sub UxStop_Click(sender As Object, e As Windows.RoutedEventArgs) Handles UxStop.Click
-        Stop
+        [Stop]
     End Sub
 End Class
